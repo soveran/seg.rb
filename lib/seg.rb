d26e2a2bed09d60263bc1fde4fc6dccf5ea00baf
Francesco Rodríguez <frodsan@protonmail.ch>
2016-05-23 14:34:45 +0200

    Add method to return capture without store.

--- lib/seg.rb
+++ lib/seg.rb
@@ -1,17 +1,17 @@
 # encoding: UTF-8
 #
 # Copyright (c) 2015 Michel Martens
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -53,6 +53,17 @@ class Seg
     @pos >= @size
   end
 
+  def succ
+    return nil if root?
+
+    len = (@path.index(SLASH, @pos) || @size) - @pos
+    res = subs(len)
+
+    move(len)
+
+    res
+  end
+
   def consume(str)
     return false if root?
 
@@ -72,13 +83,14 @@ class Seg
   end
 
   def capture(sym, store)
-    return false if root?
+    v = succ
 
-    len = (@path.index(SLASH, @pos) || @size) - @pos
+    if v
+      store[sym] = v
 
-    store[sym] = @path[@pos, len]
-    move(len)
-
-    return true
+      return true
+    else
+      return false
+    end
   end
 end
